c1cdab51cf1b78ceb421e573df9a373536fa84ef
mariklim <maria.klimenko889@gmail.com>
2021-10-19 15:04:57 +0200

    aggiunto input

--- snack2/index.php
+++ snack2/index.php
@@ -3,11 +3,42 @@
  $mailUtente = $_GET['mail'];
  $mailAge= $_GET['age'];
 
-echo strlen($nameUtente);
 
-if(strlen($nameUtente)>3 && strpos($mailUtente , '@') && strpos($mailUtente , '.') && is_numeric($mailAge)) {
+// if(strlen($nameUtente)>3 && strpos($mailUtente , '@') && strpos($mailUtente , '.') && is_numeric($mailAge)) {
+//     echo "Accesso riuscito";
+// }
+// else{
+//     echo "Accesso negato";
+// }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accesso utente</title>
+</head>
+<body>
+<h3>Accesso account</h3>
+<form action="index.php" method="get">
+Nome: <input type="text" name='name' placeholder="Inserisci il tuo nome">
+Mail: <input type="mail" name='mail' placeholder="Inserisci la tua mail">
+Età: <input type="text" name='age' placeholder="Inserisci la tua età">
+<input type="submit">
+
+</form>
+
+ <?php 
+ if(strlen($nameUtente)>3 && strpos($mailUtente , '@') && strpos($mailUtente , '.') && is_numeric($mailAge)) {  
     echo "Accesso riuscito";
 }
 else{
     echo "Accesso negato";
 }
+ ?>
+
+    
+</body>
+</html>
