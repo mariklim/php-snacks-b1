001ba8e82284e2b89a10dc371db5b881c90b5cc8
mariklim <maria.klimenko889@gmail.com>
2021-10-20 12:19:45 +0200

    aggiunto base snack 3

---
+++ snack3/index.php
@@ -0,0 +1,60 @@
+<?php
+$posts = [
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
+<ul>
+<?php
+foreach($posts as $data => $infoMessaggi){?>
+<li><h3>
+<?php echo $data;?>
+</h3>
+
+<?php foreach ($infoMessaggi as $key){
+  
+
+    echo $key['title'];
+    echo $key['author'];
+    echo $key['text'];
+
+
+  
+ } ?>
+</li>
+<?php } ?>
+</ul>
