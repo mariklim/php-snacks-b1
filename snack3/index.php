e154c701bd524f88d9c7be8a2262e3e9d70a0f5b
mariklim <maria.klimenko889@gmail.com>
2021-10-20 14:28:14 +0200

    correzione snack 3

--- snack3/index.php
+++ snack3/index.php
@@ -1,4 +1,8 @@
 <?php
+// Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
+
+// Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z
+
 $posts = [
     '10/01/2019' => [
         [
@@ -37,24 +41,36 @@ $posts = [
         ]
     ],
 ];
+
 ?>
-<ul>
-<?php
-foreach($posts as $data => $infoMessaggi){?>
-<li><h3>
-<?php echo $data;?>
-</h3>
 
-<?php foreach ($infoMessaggi as $key){
-  
-
-    echo $key['title'];
-    echo $key['author'];
-    echo $key['text'];
-
-
-  
- } ?>
-</li>
-<?php } ?>
-</ul>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Snack 3</title>
+	</head>
+	<body>
+		<h1>Blog di Samuele</h1>
+		<ul>
+			<?php foreach($posts as $date => $articles) { ?>
+			<li>
+				<h3><?= $date; ?></h3>
+				<ul>
+					<?php foreach($articles as $article) { ?>
+					<li>
+						<h4><?= $article["title"]; ?></h4>
+						<div>Scritto da: <em><?= $article["author"]; ?></em></div>
+						<p>
+							<?= $article["text"]; ?>
+						</p>
+					</li>
+					<?php } ?>
+				</ul>
+			</li>
+			<?php } ?>
+		</ul>
+	</body>
+</html>
